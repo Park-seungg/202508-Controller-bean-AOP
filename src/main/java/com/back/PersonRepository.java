397a14bcd48c75bc7b2d5704312d9090ad8957ea
Park-seungg <parkis1101@gmail.com>
2025-08-21 14:24:16 +0900

    version

--- src/main/java/com/back/PersonRepository.java
+++ src/main/java/com/back/PersonRepository.java
@@ -1,10 +1,14 @@
 package com.back;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Repository;
 
+@Repository
 @RequiredArgsConstructor
 public class PersonRepository {
     private final int version;
+    @Qualifier
 
     public long count() {
         System.out.println("PersonRepository(v%d).count() 작동".formatted(version));
