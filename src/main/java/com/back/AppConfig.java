27f25703371b5bbcc8fea8b29ff049e77fa69c93
Park-seungg <parkis1101@gmail.com>
2025-08-21 14:52:40 +0900

    @Order 도입

--- src/main/java/com/back/AppConfig.java
+++ src/main/java/com/back/AppConfig.java
@@ -3,6 +3,7 @@ package com.back;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 
 @Configuration
 public class AppConfig {
@@ -18,11 +19,13 @@ public class AppConfig {
     }
 
     @Bean
+    @Order(2)
     public ApplicationRunner myApplicationRunner() {
         return new MyApplicationRunner();
     }
 
     @Bean
+    @Order(1)
     public ApplicationRunner myApplicationRunner2() {
         return new MyApplicationRunner();
     }
